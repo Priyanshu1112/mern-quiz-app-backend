fe98153dc6c488378d9dd7170409e8e944f1db10
Priyanshu1112 <ashu1122sing@gmail.com>
2023-09-30 02:28:51 +0530

    Mongo_URL_Updated

--- server/models/db.js
+++ server/models/db.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://ashu:<qnmbbeRAMcaPipaU>@cluster0.mrkabfa.mongodb.net/"
-  )
+  .connect("mongodb+srv://ashu:qnmbbeRAMcaPipaU@cluster0.mrkabfa.mongodb.net/")
   .then(console.log("Connected to DB!!!!"))
   .catch((err) => {
     console.log(err);
