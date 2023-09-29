3f4f1f096b7261a4c5a6d4ce5745ff01ac9cad92
Priyanshu1112 <ashu1122sing@gmail.com>
2023-09-30 02:41:49 +0530

    MongoURL

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://ashu:qnmbbeRAMcaPipaU@cluster0.mrkabfa.mongodb.net/")
+  .connect(
+    "mongodb+srv://ashu:qnmbbeRAMcaPipaU@cluster0.mrkabfa.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(console.log("Connected to DB!!!!"))
   .catch((err) => {
     console.log(err);
