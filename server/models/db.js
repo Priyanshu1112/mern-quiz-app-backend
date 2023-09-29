7b78dfb8405607ae332dc226bbe833f0beb5a919
Priyanshu1112 <ashu1122sing@gmail.com>
2023-09-30 02:23:09 +0530

    Mongo Url Changed

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/quiz")
+  .connect(
+    "mongodb+srv://ashu:<qnmbbeRAMcaPipaU>@cluster0.mrkabfa.mongodb.net/"
+  )
   .then(console.log("Connected to DB!!!!"))
   .catch((err) => {
     console.log(err);
